ef379efcf5c5ae1f0ce711b0c60d061673141558
Jake <jacobcabral@gmail.com>
2019-02-07 13:13:34 -0500

    started review 3

---
+++ Review 03/src/Review03.java
@@ -0,0 +1,9 @@
+public class Review03 {
+    public static void main(String[] args) {
+        char[] name = {'J', 'a','c', 'o', 'b'};
+
+        for (char item: name) {
+            System.out.println(item);
+        }
+    }
+}
